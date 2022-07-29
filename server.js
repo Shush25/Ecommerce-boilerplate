ce5cf0972c0100a4e1b3477bcf6ef03589c1ddd7
Shush25 <shushant.kumar.singh.5@gmail.com>
2022-07-30 04:03:37 +0530

    nidea

--- server.js
+++ server.js
@@ -4,20 +4,14 @@ const router = jsonServer.router("./db.json");
 const middlewares = jsonServer.defaults({
   static: "./build",
 });
-
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 server.use(middlewares);
 server.use(
   jsonServer.rewriter({
-    "/*": "/$1",
+    "/api/*": "/$1",
   })
 );
-
 server.use(router);
-server.listen(port, () => {
-  console.log(`Server is running on ${port}`);
+server.listen(PORT, () => {
+  console.log("Server is running");
 });
-// const { exec } = require("child_process");
-// exec(
-//   "json-server -m ./node_modules/json-server-auth --watch db.json --port 5000"
-// );
