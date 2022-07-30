5b367500a2f08987f0197ecbe3a4e4f9d283d114
Shush25 <shushant.kumar.singh.5@gmail.com>
2022-07-30 07:25:21 +0530

    tring to fix

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+
 const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
+const router = jsonServer.router("db.json");
+server.db = router.db;
 const middlewares = jsonServer.defaults({
   static: "./build",
 });
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 server.use(middlewares);
 server.use(
-  jsonServer.rewriter({
+  auth.rewriter({
     "/api/*": "/$1",
   })
 );
+server.use(auth);
 server.use(router);
 server.listen(PORT, () => {
   console.log("Server is running");
